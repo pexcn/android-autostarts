52a1e0158b0910e0d79bc68fdfaa7fd07380bc3f
pexcn <pexcn97@gmail.com>
2016-10-27 15:16:08 +0000

    Update

--- app/src/main/java/com/elsdoerfer/android/autostarts/EventDetailsFragment.java
+++ app/src/main/java/com/elsdoerfer/android/autostarts/EventDetailsFragment.java
@@ -8,12 +8,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.elsdoerfer.android.autostarts.db.IntentFilterInfo;
 
 import java.util.ArrayList;
-
 
 public class EventDetailsFragment extends DialogFragment {
     public static EventDetailsFragment newInstance(IntentFilterInfo event) {
@@ -29,7 +29,7 @@ public class EventDetailsFragment extends DialogFragment {
         final IntentFilterInfo event = getArguments().getParcelable("event");
         final ListActivity activity = (ListActivity) getActivity();
 
-        View v = activity.getLayoutInflater().inflate(R.layout.receiver_info_panel, null, false);
+        View v = activity.getLayoutInflater().inflate(R.layout.receiver_info_panel, (ViewGroup) getView(), false);
         assert event != null;
         String formattedString = String.format(
                 getString(R.string.receiver_info),
@@ -48,20 +48,19 @@ public class EventDetailsFragment extends DialogFragment {
         ArrayList<CharSequence> dialogItems = new ArrayList<>();
         dialogItems.add(getResources().getString((componentIsEnabled) ? R.string.disable : R.string.enable));
 
-        return new AlertDialog.Builder(activity).setItems(
-                dialogItems.toArray(new CharSequence[dialogItems.size()]),
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        boolean doEnable = !componentIsEnabled;
-                        switch (which) {
-                            case 0:
-                                activity.addJob(event.componentInfo, doEnable);
-                                break;
-                        }
-                        dialog.dismiss();
-                    }
-                })
-                .setTitle(event.componentInfo.getLabel()).setView(v).create();
+        return new AlertDialog.Builder(activity)
+                .setItems(dialogItems.toArray(new CharSequence[dialogItems.size()]),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                boolean doEnable = !componentIsEnabled;
+                                switch (which) {
+                                    case 0:
+                                        activity.addJob(event.componentInfo, doEnable);
+                                        break;
+                                }
+                                dialog.dismiss();
+                            }
+                        }).setTitle(event.componentInfo.getLabel()).setView(v).create();
     }
 
 }
